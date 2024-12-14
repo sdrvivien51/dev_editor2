c5ac9a114469e0825ccd4379c748bb0f468acdcb
sdrvivien51 <125567464+sdrvivien51@users.noreply.github.com>
2024-12-14 14:53:52 +0000

    Assistant checkpoint: Added embed services configuration directly in editor.ts
    Assistant generated file changes:
    - client/src/lib/editor.ts: Add embed services directly in editor config
    
    ---
    
    User prompt:
    
    s'il teplait n'ajoute pas d'outil ou de fichier embed, je veux qu'ils soit dans le code editor.ts. Je dois pouvoir afficher l'mbed des que je met un lien URL de n'importe quel media

--- client/src/lib/editor.ts
+++ client/src/lib/editor.ts
@@ -37,7 +37,53 @@ export const editorConfig: Partial<EditorConfig> = {
       class: EmbedTool,
       inlineToolbar: true,
       config: {
-        services: EmbedTool.services
+        services: {
+          facebook: {
+            regex: /https?:\/\/www.facebook.com\/([^\/\?\&]*)\/(videos|posts)\/([^\/\?\&]*)/,
+            embedUrl: 'https://www.facebook.com/plugins/post.php?href=<%= remote_id %>',
+            html: '<iframe style="border: none; overflow: hidden;" width="100%" height="320" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>',
+            height: 320
+          },
+          instagram: {
+            regex: /https?:\/\/www.instagram.com\/p\/([^\/\?\&]*)/,
+            embedUrl: 'https://www.instagram.com/p/<%= remote_id %>/embed',
+            html: '<iframe width="400" height="505" style="border: none; overflow: hidden;" frameborder="0" scrolling="no" allowtransparency="true"></iframe>',
+            height: 505,
+            width: 400
+          },
+          youtube: {
+            regex: /(?:https?:\/\/)?(?:www\.)?(?:(?:youtu\.be\/)|(?:youtube\.com)\/(?:v\/|u\/\w\/|embed\/|watch))(?:(?:\?v=)?([^#&?]*))/,
+            embedUrl: 'https://www.youtube.com/embed/<%= remote_id %>',
+            html: '<iframe style="width:100%;" height="320" frameborder="0" allowfullscreen></iframe>',
+            height: 320
+          },
+          twitter: {
+            regex: /^https?:\/\/twitter\.com\/(?:#!\/)?(\w+)\/status(?:es)?\/(\d+)(?:\/.*)?$/,
+            embedUrl: 'https://platform.twitter.com/embed/Tweet.html?id=<%= remote_id %>',
+            html: '<iframe style="width:100%;" height="320" frameborder="0" allowfullscreen></iframe>',
+            height: 320
+          },
+          twitch: {
+            regex: /(?:www\.)?twitch\.tv\/(?:videos\/(\d+)|clips\/(\w+)|[^\/\?\&]*)/,
+            embedUrl: 'https://player.twitch.tv/?<%= remote_id %>&parent=' + window.location.hostname,
+            html: '<iframe style="width:100%;" height="320" frameborder="0" allowfullscreen></iframe>',
+            height: 320
+          },
+          codesandbox: {
+            regex: /https?:\/\/codesandbox\.io\/(?:s|p\/devbox)\/([^\/\?\&]*)/,
+            embedUrl: 'https://codesandbox.io/embed/<%= remote_id %>',
+            html: '<iframe style="width:100%;" height="500" frameborder="0" allowfullscreen></iframe>',
+            height: 500
+          },
+          codepen: {
+            regex: /https?:\/\/codepen\.io\/([^\/\?\&]*)\/pen\/([^\/\?\&]*)/,
+            embedUrl: 'https://codepen.io/<%= remote_id %>?height=300&theme-id=0&default-tab=css,result&embed-version=2',
+            html: '<iframe height="300" scrolling="no" frameborder="no" allowtransparency="true" allowfullscreen="true" style="width:100%;"></iframe>',
+            height: 300,
+            width: 600,
+            id: (groups) => groups.join('/embed/')
+          }
+        }
       }
     },
     image: {
