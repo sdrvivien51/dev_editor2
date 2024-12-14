782412bbda26b73b0a834755625a6721f8358ad9
sdrvivien51 <125567464+sdrvivien51@users.noreply.github.com>
2024-12-14 23:06:06 +0000

    Assistant checkpoint: Added embed tool with automatic link conversion
    Assistant generated file changes:
    - client/src/components/editor/EmbedTool.ts: Create embed tool for editor.js
    - client/src/components/editor/editor.tsx: Add EmbedTool to editor config
    
    ---
    
    User prompt:
    
    I'd like to create an embed features in my editor.js. In this editor. I want being able to paste a link youtube for example and display the embed iframe in my editor. Please write the code to create this. I want to also to remove the choice in the selector, i dont what to click in the selector, i want just being able to insert link and then display the embed. It should be only coded in the editor. I've renamed him tsx because and change him to an another place.

---
+++ client/src/components/editor/EmbedTool.ts
@@ -0,0 +1,122 @@
+
+import { API, BlockTool, BlockToolData } from '@editorjs/editorjs';
+
+interface EmbedData {
+  service?: string;
+  url?: string;
+  embed?: string;
+  width?: number;
+  height?: number;
+  caption?: string;
+}
+
+interface Pattern {
+  regex: RegExp;
+  embedUrl: string;
+  html: string;
+  height: number;
+  width?: number;
+  id?: (groups: string[]) => string;
+}
+
+export default class EmbedTool implements BlockTool {
+  private api: API;
+  private data: EmbedData;
+  private services: { [key: string]: Pattern };
+
+  static get toolbox() {
+    return {
+      title: 'Embed',
+      icon: '<svg width="13" height="14" viewBox="0 0 13 14"><path d="M8.567 13.629c.728.464 1.581.65 2.41.558l-.873.873A3.722 3.722 0 1 1 4.84 9.794L6.694 7.94a3.722 3.722 0 0 1 5.256-.008L10.484 9.4a5.209 5.209 0 0 1-.017.016 1.625 1.625 0 0 0-2.29.009l-1.854 1.854a1.626 1.626 0 0 0 2.244 2.35zm2.766-7.358a3.722 3.722 0 0 0-5.256.008L7.422 4.824a5.2 5.2 0 0 1 .017-.016 1.625 1.625 0 0 0 2.29-.009l1.854-1.854a1.626 1.626 0 0 0-2.244-2.35L7.783.987c-.728-.464-1.581-.65-2.41-.558l.873-.873a3.722 3.722 0 1 1 5.256 5.266L9.649 6.674z"/></svg>'
+    };
+  }
+
+  constructor({ data, api }: { data: BlockToolData<EmbedData>; api: API }) {
+    this.api = api;
+    this.data = data;
+
+    this.services = {
+      youtube: {
+        regex: /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/watch\?v=|youtu\.be\/)([^\/\?\&]*)/,
+        embedUrl: 'https://www.youtube.com/embed/<%= remote_id %>',
+        html: '<iframe style="width:100%;" height="320" frameborder="0" allowfullscreen></iframe>',
+        height: 320
+      },
+      twitter: {
+        regex: /^https?:\/\/twitter\.com\/(?:#!\/)?(\w+)\/status(?:es)?\/(\d+)(?:\/.*)?$/,
+        embedUrl: 'https://platform.twitter.com/embed/Tweet.html?id=<%= remote_id %>',
+        html: '<iframe style="width:100%;" height="320" frameborder="0" allowfullscreen></iframe>',
+        height: 320
+      },
+      instagram: {
+        regex: /https?:\/\/www.instagram.com\/p\/([^\/\?\&]*)/,
+        embedUrl: 'https://www.instagram.com/p/<%= remote_id %>/embed',
+        html: '<iframe width="400" height="505" style="border: none; overflow: hidden;" frameborder="0" scrolling="no" allowtransparency="true"></iframe>',
+        height: 505,
+        width: 400
+      }
+    };
+  }
+
+  render() {
+    const wrapper = document.createElement('div');
+    wrapper.classList.add('embed-tool');
+
+    if (!this.data.embed) {
+      const input = document.createElement('input');
+      input.placeholder = 'Paste your link here...';
+      input.classList.add('embed-tool__input');
+      
+      input.addEventListener('paste', (event) => {
+        event.preventDefault();
+        const text = event.clipboardData?.getData('text');
+        if (text) {
+          this.embedUrlHandle(text, wrapper);
+        }
+      });
+
+      wrapper.appendChild(input);
+    } else {
+      wrapper.innerHTML = this.data.embed;
+    }
+
+    return wrapper;
+  }
+
+  private embedUrlHandle(url: string, wrapper: HTMLElement) {
+    for (const service in this.services) {
+      const pattern = this.services[service];
+      const match = url.match(pattern.regex);
+
+      if (match) {
+        const remote_id = pattern.id ? pattern.id(match.slice(1)) : match[1];
+        const embedUrl = pattern.embedUrl.replace('<%= remote_id %>', remote_id);
+        
+        const html = pattern.html
+          .replace('src=""', `src="${embedUrl}"`)
+          .replace('height="320"', `height="${pattern.height}"`);
+        
+        if (pattern.width) {
+          html.replace('width="100%"', `width="${pattern.width}"`);
+        }
+
+        this.data = {
+          service,
+          url,
+          embed: html
+        };
+
+        wrapper.innerHTML = html;
+        return;
+      }
+    }
+  }
+
+  save(blockContent: HTMLElement) {
+    return this.data;
+  }
+
+  validate(savedData: BlockToolData<EmbedData>) {
+    return savedData.embed?.length > 0;
+  }
+}
---
+++ client/src/components/editor/editor.tsx
@@ -0,0 +1,121 @@
+
+import Header from '@editorjs/header';
+import List from '@editorjs/list';
+import Image from '@editorjs/image';
+import { type EditorConfig } from '@editorjs/editorjs';
+import { supabase } from './supabase';
+import ChartTool from '@/components/editor/ChartTool';
+import TradingViewTool from '@/components/editor/TradingViewTool';
+import EmbedTool from './EmbedTool';
+
+export const editorConfig: Partial<EditorConfig> = {
+  tools: {
+    header: {
+      class: Header,
+      inlineToolbar: true,
+      config: {
+        levels: [1, 2, 3, 4],
+        defaultLevel: 2
+      }
+    },
+    list: {
+      class: List,
+      inlineToolbar: true,
+      config: {
+        defaultStyle: 'unordered'
+      }
+    },
+    chart: {
+      class: ChartTool,
+      inlineToolbar: true,
+    },
+    tradingview: {
+      class: TradingViewTool,
+      inlineToolbar: false,
+    },
+    embed: {
+      class: EmbedTool,
+      inlineToolbar: true,
+      config: {
+        services: {
+          facebook: {
+            regex: /https?:\/\/www.facebook.com\/([^\/\?\&]*)\/(videos|posts)\/([^\/\?\&]*)/,
+            embedUrl: 'https://www.facebook.com/plugins/post.php?href=<%= remote_id %>',
+            html: '<iframe style="border: none; overflow: hidden;" width="100%" height="320" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>',
+            height: 320
+          },
+          instagram: {
+            regex: /https?:\/\/www.instagram.com\/p\/([^\/\?\&]*)/,
+            embedUrl: 'https://www.instagram.com/p/<%= remote_id %>/embed',
+            html: '<iframe width="400" height="505" style="border: none; overflow: hidden;" frameborder="0" scrolling="no" allowtransparency="true"></iframe>',
+            height: 505,
+            width: 400
+          },
+          youtube: true,
+          twitter: {
+            regex: /^https?:\/\/twitter\.com\/(?:#!\/)?(\w+)\/status(?:es)?\/(\d+)(?:\/.*)?$/,
+            embedUrl: 'https://platform.twitter.com/embed/Tweet.html?id=<%= remote_id %>',
+            html: '<iframe style="width:100%;" height="320" frameborder="0" allowfullscreen></iframe>',
+            height: 320
+          },
+          twitch: {
+            regex: /(?:www\.)?twitch\.tv\/(?:videos\/(\d+)|clips\/(\w+)|[^\/\?\&]*)/,
+            embedUrl: 'https://player.twitch.tv/?<%= remote_id %>&parent=' + window.location.hostname,
+            html: '<iframe style="width:100%;" height="320" frameborder="0" allowfullscreen></iframe>',
+            height: 320
+          },
+          codesandbox: {
+            regex: /https?:\/\/codesandbox\.io\/(?:s|p\/devbox)\/([^\/\?\&]*)/,
+            embedUrl: 'https://codesandbox.io/embed/<%= remote_id %>',
+            html: '<iframe style="width:100%;" height="500" frameborder="0" allowfullscreen></iframe>',
+            height: 500
+          },
+          codepen: {
+            regex: /https?:\/\/codepen\.io\/([^\/\?\&]*)\/pen\/([^\/\?\&]*)/,
+            embedUrl: 'https://codepen.io/<%= remote_id %>?height=300&theme-id=0&default-tab=css,result&embed-version=2',
+            html: '<iframe height="300" scrolling="no" frameborder="no" allowtransparency="true" allowfullscreen="true" style="width:100%;"></iframe>',
+            height: 300,
+            width: 600,
+            id: (groups) => groups.join('/embed/')
+          }
+        }
+      }
+    },
+    image: {
+      class: Image,
+      config: {
+        uploader: {
+          async uploadByFile(file: File) {
+            try {
+              const { data, error } = await supabase.storage
+                .from('blog-images')
+                .upload(`${Date.now()}-${file.name}`, file);
+
+              if (error) throw error;
+
+              const { data: { publicUrl } } = supabase.storage
+                .from('blog-images')
+                .getPublicUrl(data.path);
+
+              return {
+                success: 1,
+                file: {
+                  url: publicUrl,
+                }
+              };
+            } catch (error) {
+              console.error('Image upload failed:', error);
+              return {
+                success: 0,
+                error: 'Image upload failed'
+              };
+            }
+          }
+        }
+      }
+    }
+  },
+  placeholder: 'Let\'s write an awesome story!',
+  autofocus: true,
+  inlineToolbar: true,
+};
